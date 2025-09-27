7bd5289fb25199c920ced5e0a3bb36ee8a05a968
MuraliKrishna369 <muralikrishnapogiri369@gmail.com>
2025-09-27 20:37:09 +0530

    Solved CORS Problem

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const cors = require("cors")
+
+
+const app = express()
+app.use(cors());
+
+app.get("/restaurants", async (req, res) => {
+  try {
+    console.log("I am hitted")
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4356626&lng=78.44087999999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0", // mimic a browser
+          "Accept": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(7777, () => {
+    console.log("Server running on port 7777")
+})
